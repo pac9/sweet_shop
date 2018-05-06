e050be3cef000ab1b36779261632bfe570686f83
pac9 <pac9@btinternet.com>
2018-05-06 09:52:32 +0100

    set up sql file with two tables

---
+++ db/sweet_shop.sql
@@ -0,0 +1,19 @@
+DROP TABLE products;
+DROP TABLE suppliers;
+
+CREATE TABLE suppliers(
+  id SERIAL4 PRIMARY KEY,
+  name VARCHAR(255) not null,
+  telephone_number VARCHAR(255)
+);
+
+CREATE TABLE products(
+  id SERIAL4 PRIMARY KEY,
+  product VARCHAR(255),
+  description VARCHAR(255),
+  stock_quantity INT4,
+  buying_cost INT4,
+  selling_price INT4,
+  supplier_id INT4 REFERENCES suppliers(id),
+  comments VARCHAR(255)
+);
