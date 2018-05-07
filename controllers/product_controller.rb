c840fe178c46e0e057c8ef83c386804f47339aeb
pac9 <pac9@btinternet.com>
2018-05-07 19:11:51 +0100

    check views will load a view in web browser

--- controllers/product_controller.rb
+++ controllers/product_controller.rb
@@ -1,3 +1,8 @@
 require('sinatra')
 require('sinatra/contrib/all')
 require_relative('../models/product')
+
+get '/products' do
+  @products = Product.all()
+  erb ( :"products/index" )
+end
--- controllers/supplier_controller.rb
+++ controllers/supplier_controller.rb
@@ -1,3 +1,8 @@
 require('sinatra')
 require('sinatra/contrib/all')
 require_relative('../models/supplier')
+
+get '/suppliers' do
+  @suppliers = Supplier.all()
+  erb ( :"suppliers/index" )
+end
