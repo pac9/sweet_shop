790c6b48a0b4a76f6522a645cbc6ca2d6d812b04
pac9 <pac9@btinternet.com>
2018-05-07 10:33:11 +0100

    TDD in supplier spec working

---
+++ models/specs/product_spec.rb
@@ -0,0 +1,9 @@
+require('minitest/autorun')
+require('minitest/rg')
+require_relative('../product')
+
+class TestProduct < Minitest::Test
+
+
+
+end
---
+++ models/specs/supplier_spec.rb
@@ -0,0 +1,22 @@
+require('minitest/autorun')
+require('minitest/rg')
+require_relative('../supplier')
+
+class TestSupplier < Minitest::Test
+
+  def setup
+    options = {"name" => "MaKCandy", "telephone_number" => "0131 123 3456"}
+
+    @name = Supplier.new(options)
+    @telephone_number = Supplier.new(options)
+  end
+
+  def test_name()
+    assert_equal("MaKCandy", @name.name())
+  end
+
+  def test_telephone_number
+    assert_equal("0131 123 3456", @telephone_number.telephone_number())
+  end
+
+end
